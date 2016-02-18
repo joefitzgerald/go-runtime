811522e708c979c3b6fc600796b4ac6ac59db4cb
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-02-18 13:42:29 -0700

    Try To Resolve Lack Of Test Output

--- build-package-remote.ps1
+++ build-package-remote.ps1
@@ -7,7 +7,7 @@ if ($env:ATOM_CHANNEL) {
     $script:ATOM_DIRECTORY_NAME += "$script:ATOM_CHANNEL.substring(0,1).toupper()"
     $script:ATOM_DIRECTORY_NAME += "$script:ATOM_CHANNEL.substring(1).tolower()"
 }
-$script:ATOM_SCRIPT_PATH = "$PSScriptRoot\$script:ATOM_DIRECTORY_NAME\resources\cli\atom.cmd"
+$script:ATOM_SCRIPT_PATH = "$PSScriptRoot\$script:ATOM_DIRECTORY_NAME\atom.exe"
 $script:APM_SCRIPT_PATH = "$PSScriptRoot\$script:ATOM_DIRECTORY_NAME\resources\app\apm\bin\apm.cmd"
 
 
@@ -170,7 +170,7 @@ function RunSpecs() {
         ExitWithCode -exitcode 1
     }
     Write-Host "Running specs..."
-    & "$ATOM_SCRIPT_PATH" --test spec
+    & "$APM_SCRIPT_PATH" test .
     if ($LASTEXITCODE -ne 0) {
         ExitWithCode -exitcode $LASTEXITCODE
     }
