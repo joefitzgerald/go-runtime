ec0d980582c7a87ba301de60d6a952202fef68b1
Joe Fitzgerald <jfitzgerald@pivotal.io>
2016-02-18 15:18:11 -0700

    Fix Issue With Specifying Channel

--- build-package-remote.ps1
+++ build-package-remote.ps1
@@ -1,11 +1,11 @@
 Set-StrictMode -Version Latest
 $script:ATOM_CHANNEL = "stable"
 $script:ATOM_DIRECTORY_NAME = "Atom"
-if ($env:ATOM_CHANNEL) {
+if ($env:ATOM_CHANNEL -and ($env:ATOM_CHANNEL.tolower() -ne "stable")) {
     $script:ATOM_CHANNEL = "$env:ATOM_CHANNEL"
     $script:ATOM_DIRECTORY_NAME = "$script:ATOM_DIRECTORY_NAME "
-    $script:ATOM_DIRECTORY_NAME += "$script:ATOM_CHANNEL.substring(0,1).toupper()"
-    $script:ATOM_DIRECTORY_NAME += "$script:ATOM_CHANNEL.substring(1).tolower()"
+    $script:ATOM_DIRECTORY_NAME += $script:ATOM_CHANNEL.substring(0,1).toupper()
+    $script:ATOM_DIRECTORY_NAME += $script:ATOM_CHANNEL.substring(1).tolower()
 }
 $script:ATOM_SCRIPT_PATH = "$PSScriptRoot\$script:ATOM_DIRECTORY_NAME\atom.exe"
 $script:APM_SCRIPT_PATH = "$PSScriptRoot\$script:ATOM_DIRECTORY_NAME\resources\app\apm\bin\apm.cmd"
